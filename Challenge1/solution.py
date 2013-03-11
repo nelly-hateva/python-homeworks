7b54d73fb8babb8de5493869ef907e1d6357e381
nelly-hateva <nelly.hateva@gmail.com>
2013-03-12 00:05:52 +0200

    Name changes

--- Challenge1/solution.py
+++ Challenge1/solution.py
@@ -1,8 +1,10 @@
-def calculate_coins(sum_):
-    COINS_NOMINAL = (100, 50, 20, 10, 5, 2, 1)
-    coin_distribution = {}
-    sum_ = round(sum_*100)
-    for coin in COINS_NOMINAL:
-        coin_distribution[coin] = sum_ // coin
-        sum_ %= coin
-    return coin_distribution
+COINS = (100, 50, 20, 10, 5, 2, 1)
+
+
+def calculate_coins(amount):
+    coins_representation = {}
+    amount_in_coins = round(amount*100)
+    for coin in sorted(COINS, reverse=True):
+        coins_representation[coin] = amount_in_coins // coin
+        amount_in_coins %= coin
+    return coins_representation
