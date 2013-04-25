2cb2ef4df287462f4c0c84309341fd760c620cf2
Nelly Hateva <n.hateva@antipodes.bg>
2013-04-25 18:32:53 +0300

    DRY code

--- Homeworks/03/sample_test.py
+++ Homeworks/03/sample_test.py
@@ -1,4 +1,6 @@
 import unittest
+
+
 import solution
 
 
@@ -7,61 +9,46 @@ class PersonTest(unittest.TestCase):
     def setUp(self):
         self.adam = solution.Person(name='Adam', gender='M', birth_year=0)
         self.eva = solution.Person(name='Eva', gender='F', birth_year=0)
-        self.first_son = solution.Person(name='Kain', gender='M',
-                                         birth_year=20, father=self.adam,
+        self.first_son = solution.Person(name='Kain',
+                                         gender='M',
+                                         birth_year=20,
+                                         father=self.adam,
                                          mother=self.eva)
-        self.first_daughter = solution.Person(name='Pepa', gender='F',
-                                              birth_year=22, father=self.adam,
+        self.first_daughter = solution.Person(name='Pepa',
+                                              gender='F',
+                                              birth_year=22,
+                                              father=self.adam,
                                               mother=self.eva)
-        self.evas_secret_son = solution.Person(name='Ali Raza', gender='M',
-                                               birth_year=25, mother=self.eva)
-        self.third_daughter = solution.Person(name='Ferhunde', gender='F',
-                                              birth_year=45,
-                                              father=self.evas_secret_son)
 
     def tearDown(self):
         del self.adam
         del self.eva
         del self.first_son
         del self.first_daughter
-        del self.evas_secret_son
-        del self.third_daughter
 
     def test_attributes(self):
         self.assertIn('name', dir(self.adam))
         self.assertIn('gender', dir(self.adam))
         self.assertIn('birth_year', dir(self.adam))
+        self.assertIn('get_sisters', dir(self.adam))
+        self.assertIn('get_brothers', dir(self.adam))
+        self.assertIn('children', dir(self.adam))
 
     def test_has_sister(self):
         self.assertEqual(self.first_son.get_sisters(), [self.first_daughter])
 
     def test_has_brother(self):
-        self.assertEqual(self.first_son.get_brothers(), [self.evas_secret_son])
+        self.assertEqual(self.first_daughter.get_brothers(), [self.first_son])
 
-    def test_common_mother_sisters(self):
-        self.assertEqual(self.evas_secret_son.get_sisters(), [
-            self.first_daughter
-        ])
-
-    def test_common_mother_brothers(self):
-        self.assertEqual(self.evas_secret_son.get_brothers(), [
-            self.first_son
-        ])
-
-    def test_has_no_sisters(self):
-        self.assertEqual(self.adam.get_sisters(), [])
+    def test_has_no_sister(self):
         self.assertEqual(self.first_daughter.get_sisters(), [])
 
-    def test_has_no_brothers(self):
-        self.assertEqual(self.adam.get_brothers(), [])
+    def test_has_no_brother(self):
+        self.assertEqual(self.first_son.get_brothers(), [])
 
-    def test_has_no_self_in_sisters(self):
-        self.assertNotIn(self.first_daughter,
-                         self.first_daughter.get_sisters())
-
-    def test_has_no_self_in_brothers(self):
-        self.assertNotIn(self.first_son,
-                         self.first_son.get_brothers())
+    def test_children(self):
+        self.assertEqual(
+            self.adam.children(), [self.first_son, self.first_daughter])
 
     def test_father_has_son(self):
         self.assertEqual(self.adam.children(gender='M'), [self.first_son])
@@ -69,12 +56,19 @@ class PersonTest(unittest.TestCase):
     def test_father_has_daughter(self):
         self.assertEqual(self.adam.children(gender='F'), [self.first_daughter])
 
+    def test_mother_has_son(self):
+        self.assertEqual(self.eva.children(gender='M'), [self.first_son])
+
+    def test_mother_has_daughter(self):
+        self.assertEqual(self.eva.children(gender='F'), [self.first_daughter])
+
     def test_direct_successor(self):
         self.assertTrue(self.adam.is_direct_successor(self.first_son))
-        self.assertFalse(self.adam.is_direct_successor(self.adam))
-        self.assertTrue(self.eva.is_direct_successor(self.evas_secret_son))
-        self.assertFalse(self.adam.is_direct_successor(self.evas_secret_son))
-        self.assertFalse(self.adam.is_direct_successor(self.third_daughter))
+
+    def test_indirect_successor(self):
+        irrelevant = solution.Person(
+            name='John Doe', gender='M', birth_year=1990)
+        self.assertFalse(self.adam.is_direct_successor(irrelevant))
 
 if __name__ == '__main__':
     unittest.main()
--- Homeworks/03/solution.py
+++ Homeworks/03/solution.py
@@ -3,38 +3,39 @@ class Person:
         self.name, self.gender, self.birth_year = name, gender, birth_year
         self.father, self.mother = father, mother
         if(self.father is not None):
-            self.father.successor.append(self)
+            self.father.successors.append(self)
         if(self.mother is not None):
-            self.mother.successor.append(self)
-        self.successor = []
+            self.mother.successors.append(self)
+        self.successors = []
 
     def children(self, gender=None):
-        if(gender == 'M'):
-            return [child for child in self.successor if child.gender == 'M']
-        elif(gender == 'F'):
-            return [child for child in self.successor if child.gender == 'F']
+        if(gender is None):
+            return self.successors
         else:
-            return self.successor
+            return \
+                [child for child in self.successors if child.gender == gender]
 
     def is_direct_successor(self, child):
-        return child in self.successor
+        return child in self.successors
+
+    def get_relatives_by_parent(self, parent, gender):
+        if parent == 'mother':
+            mothers_children = self.mother.children(gender)
+            return [child for child in mothers_children if self is not child]
+        if parent == 'father':
+            fathers_children = self.father.children(gender)
+            return [child for child in fathers_children if self is not child]
+
+    def get_relatives(self, gender):
+        from_mother_side, from_father_side = [], []
+        if(self.mother is not None):
+            from_mother_side = self.get_relatives_by_parent('mother', gender)
+        if(self.father is not None):
+            from_father_side = self.get_relatives_by_parent('father', gender)
+        return list(set(from_mother_side + from_father_side))
 
     def get_sisters(self):
-        sisters_from_mother, sisters_from_father = [], []
-        if(self.mother is not None):
-            sisters_from_mother = \
-            [child for child in self.mother.children('F') if self is not child]
-        if(self.father is not None):
-            sisters_from_father = \
-            [child for child in self.father.children('F') if self is not child]
-        return list(set(sisters_from_mother + sisters_from_father))
+        return self.get_relatives('F')
 
     def get_brothers(self):
-        brothers_from_mother, brothers_from_father = [], []
-        if(self.mother is not None):
-            brothers_from_mother = \
-            [child for child in self.mother.children('M') if self is not child]
-        if(self.father is not None):
-            brothers_from_father = \
-            [child for child in self.father.children('M') if self is not child]
-        return list(set(brothers_from_mother + brothers_from_father))
+        return self.get_relatives('M')
