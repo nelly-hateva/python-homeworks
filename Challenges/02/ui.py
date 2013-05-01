7ba80024abab158ecfafba052d948a8733090f45
Nelly Hateva <n.hateva@antipodes.bg>
2013-05-01 18:10:28 +0300

    User interface

---
+++ Challenges/02/ui.py
@@ -0,0 +1,32 @@
+import turtle
+from solution import dragon_fractal
+
+
+LINE_LENGTH = 5  # 1
+DELAY_MS = 0
+REPEAT_TIMES = 11  # 15
+
+
+def str_to_heading(str_):
+    if str_ == 'up':
+        return 90
+    elif str_ == 'left':
+        return 180
+    elif str_ == 'right':
+        return 0
+    elif str_ == 'down':
+        return 270
+    else:
+        raise Exception('Unknown heading {}'.format(str_))
+
+turtle.hideturtle()
+turtle.speed(0)
+turtle.delay(DELAY_MS)
+
+dragon_iterator = iter(dragon_fractal())
+for _ in range(2 ** REPEAT_TIMES):
+    step_direction = next(dragon_iterator)
+    turtle.setheading(str_to_heading(step_direction))
+    turtle.forward(LINE_LENGTH)
+
+turtle.done()
