342923bb37c9a55042ef833fd6a4826562baa969
Nelly Hateva <n.hateva@antipodes.bg>
2013-05-08 10:57:15 +0300

    Commit

---
+++ Challenges/02/solution.py
@@ -0,0 +1,21 @@
+from collections import deque as deque
+from itertools import count as count
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+ROTATED = {UP: LEFT, DOWN: RIGHT, LEFT: DOWN, RIGHT: UP}
+
+
+def dragon_fractal():
+    grapfic = deque()
+    yield UP
+    grapfic.append(UP)
+
+    while True:
+        for j in count(0):
+            for i in range(2 ** j - 1, -1, -1):
+                line = grapfic[i]
+                yield ROTATED[line]
+                grapfic.append(ROTATED[line])
